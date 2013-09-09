fa9825b12af644782e353aa037645e38f384b44e
zhibinw <zhibinwang.q@gmail.com>
2013-09-10 07:47:25 +0800

    add gravatar images.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def gravata_for (user)
+    gravata_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravata_url = "https://secure.gravatar.com/avatar/#{gravata_id}"
+    image_tag gravata_url, alt: user.name, class: "gravata"
+  end
 end
