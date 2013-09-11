ea2d5ded5b4decc8ae1d2e344d2034db40b16037
zhibinw <zhibinwang.q@gmail.com>
2013-09-11 16:43:27 +0800

    add user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
   def new
   end
+  def create
+    @user = User.new(params.require(:user).permit(:name,:email,:password,:password_confirmation))
+    if @user.save
+      puts 'success'
+      redirect_to @user
+    else
+      puts 'fail'
+      render 'new'
+    end
+  end
   def show
     @user = User.find(params[:id])
   end
