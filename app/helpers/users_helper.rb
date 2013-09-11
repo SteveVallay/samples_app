a675e9dc15d57e94f9061d7da8a0c005269748bd
zhibinw <zhibinwang.q@gmail.com>
2013-09-11 17:11:03 +0800

    add size option for gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,8 @@
 module UsersHelper
-  def gravata_for (user)
+  def gravata_for (user, options = { size: 50})
+    size = options[:size]
     gravata_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravata_url = "https://secure.gravatar.com/avatar/#{gravata_id}"
+    gravata_url = "https://secure.gravatar.com/avatar/#{gravata_id}?s=#{size}"
     image_tag gravata_url, alt: user.name, class: "gravata"
   end
 end
