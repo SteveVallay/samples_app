6c909aa9a4cf4560e5b22149144488b0c8daf7fb
zhibinw <zhibinwang.q@gmail.com>
2013-09-13 21:50:01 +0800

    sign in after create new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params.require(:user).permit(:name,:email,:password,:password_confirmation))
     if @user.save
+      sign_in(@user)
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
