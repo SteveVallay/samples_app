fcb2601d950e622e22266ab385f4d65cee36f49c
zhibinw <zhibinwang.q@gmail.com>
2013-09-13 21:43:33 +0800

    add sign-in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ def create
   user = User.find_by_email(params[:session][:email])
   if user && user.authenticate(params[:session][:password])
     #comments
+    sign_in(user)
+    redirect_to use
   else
     flash.now[:error] = " invalid email/password combination"
     render 'new'
