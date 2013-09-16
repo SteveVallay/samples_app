8b410a47a1d652c2e430f4e17263e9df4533769c
zhibinw <zhibinwang.q@gmail.com>
2013-09-16 14:43:48 +0800

    check current user for update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:edit,:update]
+  before_filter :correct_user, only: [:edit, :update]
   def new
   end
   def create
@@ -17,9 +18,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def signed_in_user
-    redirect_to signin_path , notice: "Please sign in." unless signed_in?
-  end
   def edit
     @user = User.find(params[:id])
   end
@@ -34,4 +32,13 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
+  private 
+    def signed_in_user
+      redirect_to signin_path , notice: "Please sign in." unless signed_in?
+    end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
