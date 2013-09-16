3747ba359f756bd7f5b6cb0bc971c5e50afe97e8
zhibinw <zhibinwang.q@gmail.com>
2013-09-16 14:19:33 +0800

    add edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,4 +20,18 @@ class UsersController < ApplicationController
   def signed_in_user
     redirect_to signin_path , notice: "Please sign in." unless signed_in?
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])  
+    if @user.update_attributes(params.require(:user).permit(:name,:email,:password,:password_confirmation))
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+      #comment
+    else
+      render 'edit'
+    end
+  end
 end
