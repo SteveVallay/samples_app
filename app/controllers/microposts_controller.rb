c325ba63a106fad3d13b65521c518f576dcfe06a
zhibinw <zhibinwang.q@gmail.com>
2013-09-21 14:39:04 +0800

    destroy micropost

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_filter  :signed_in_user, only: [:create, :destroy]
+  before_filter :correct_user ,only: :destroy
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
@@ -11,6 +12,8 @@ class MicropostsController < ApplicationController
     end
   end
   def destroy
+    @micropost.destroy
+    redirect_to root_url
   end
   def index
   end
@@ -18,5 +21,9 @@ class MicropostsController < ApplicationController
 
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+    def correct_user
+      @micropost=current_user.microposts.find_by_id(params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 end
